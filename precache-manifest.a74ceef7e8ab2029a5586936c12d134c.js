e82a39208f4163ccdd7b3878a21684b4c2924e41
OlgaTitarenko <kizuneworld@gmail.com>
2019-10-04 18:30:33 +0300

    Updates

---
+++ precache-manifest.a74ceef7e8ab2029a5586936c12d134c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ffcdf866279e88a7e2ab698d01d62e",
+    "url": "/klass-room/index.html"
+  },
+  {
+    "revision": "701ed105d796e3e52db1",
+    "url": "/klass-room/static/css/main.05aaec2d.chunk.css"
+  },
+  {
+    "revision": "245c2b2be0a5b3d59444",
+    "url": "/klass-room/static/js/2.fb8dd7cd.chunk.js"
+  },
+  {
+    "revision": "701ed105d796e3e52db1",
+    "url": "/klass-room/static/js/main.e23d2438.chunk.js"
+  },
+  {
+    "revision": "937e3989afff7bbcd330",
+    "url": "/klass-room/static/js/runtime-main.04e7a6b3.js"
+  },
+  {
+    "revision": "66e0d3ae13bc653e29d01e7eea876256",
+    "url": "/klass-room/static/media/header.66e0d3ae.png"
+  }
+]);
